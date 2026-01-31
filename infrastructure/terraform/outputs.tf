94b6810d000ef4108160ac4be285609e663ebbe9
Scott Densmore <scottdensmore@mac.com>
2026-01-30 21:14:34 -0800

    feat(infra): Merge root and chat service Terraform configurations

---
+++ infrastructure/terraform/outputs.tf
@@ -0,0 +1,40 @@
+output "project_id" {
+  value = var.project_id
+}
+
+output "region" {
+  value = var.region
+}
+
+output "db_instance_name" {
+  value = google_sql_database_instance.postgres.name
+}
+
+output "db_name" {
+  value = google_sql_database.database.name
+}
+
+output "db_user" {
+  value = google_sql_user.users.name
+}
+
+output "db_password" {
+  value     = random_password.db_password.result
+  sensitive = true
+}
+
+output "firestore_database_name" {
+  value = google_firestore_database.customer_db.name
+}
+
+output "container_registry_url" {
+  value = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.container_registry.repository_id}"
+}
+
+output "storage_bucket_name" {
+  value = google_storage_bucket.app_storage.name
+}
+
+output "app_service_account_email" {
+  value = google_service_account.app_service_account.email
+}
---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region for resources"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "firestore_location" {
+  description = "The location for Firestore database"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "environment_name" {
+  description = "Name of the environment (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "force_destroy" {
+  description = "Whether to force destroy storage buckets"
+  type        = bool
+  default     = false
+}
+
+variable "enable_deletion_protection" {
+  description = "Enable deletion protection for databases"
+  type        = bool
+  default     = true
+}
+
+variable "alert_email" {
+  description = "Email address for monitoring alerts"
+  type        = string
+  default     = ""
+}
